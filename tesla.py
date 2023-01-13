f82c9d7c26c90da7c87e3511a5082c45c1ac9327
Eric Kanguzu <ekanguzu@olsson.com>
2023-01-13 14:31:32 -0600

    first version

--- tesla.py
+++ tesla.py
@@ -1,4 +1,15 @@
 
+# 1. Write a function called checkDriverAge().
+# Whenever you call this function, you will get prompted for age.
+# Notice the benefit in having checkDriverAge() instead of copying
+# and pasting the function everytime?
+
+# 2. Instead of using the input(). Now, make the checkDriverAge() function
+# accept an argument of age, so that if you enter:
+# checkDriverAge(92);
+# it returns "Powering On. Enjoy the ride!"
+# also make it so that the default age is set to 0
+# if no argument is given
 
 def check_driver_age(age=0):
     # age = input("What is your age?: ")
@@ -12,15 +23,3 @@ def check_driver_age(age=0):
 
 
 check_driver_age(25)
-
-# 1. Wrap the above code in a function called checkDriverAge().
-# Whenever you call this function, you will get prompted for age.
-# Notice the benefit in having checkDriverAge() instead of copying
-# and pasting the function everytime?
-
-# 2. Instead of using the input(). Now, make the checkDriverAge() function
-# accept an argument of age, so that if you enter:
-# checkDriverAge(92);
-# it returns "Powering On. Enjoy the ride!"
-# also make it so that the default age is set to 0
-# if no argument is given
