6f44ba66cab41a31e004ff7573f0645465331078
Eric Kanguzu <ekanguzu@olsson.com>
2023-01-26 08:37:59 -0600

    first version

---
+++ convert_to_celsius.py
@@ -0,0 +1,18 @@
+# Let's take a break from working on "Fantasy Quest" for a moment. Instead, we will use what we've learned
+# to build a function we could use in the real world.
+# Write a function called to_celsius that converts a temperature in Fahrenheit to Celsius.
+
+# celsius = 5/9 * (fahrenheit-32)
+def to_celsius(f):
+    return 5 / 9 * (f - 32)
+
+
+def test(f):
+    c = round(to_celsius(f), 2)
+    print(f"{f} degrees fahrenheit is {c} degrees celsius")
+
+
+test(100)
+test(88)
+test(104)
+test(112)
